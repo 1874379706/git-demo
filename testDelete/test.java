9de11bfb29589a8b18b6eb1cf6ea8d2a52cf23b6
1874379706 <1874379706@qq.com>
2023-11-27 10:56:51 +0800

    add test.java

---
+++ testDelete/test.java
@@ -0,0 +1,11 @@
+package testDelete;
+/**
+ * @Author WXY
+ * @Description TODO
+ * @Date 2023-11-27 10:55
+ * @Version 1.0
+ */public class test {
+    public static void main(String[] args) {
+        System.out.println("+++++++++++++");
+    }
+}

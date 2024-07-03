8684a715758e6910f66b9d5be2508040819e72d8
wangxingyu <1874379706@qq.com>
2024-07-03 16:44:44 +0800

    换行

--- testDelete/test.java
+++ testDelete/test.java
@@ -4,7 +4,8 @@ package testDelete;
  * @Description TODO
  * @Date 2023-11-27 10:55
  * @Version 1.0
- */public class test {
+ */
+public class test {
     public static void main(String[] args) {
         System.out.println("+++++++++++++");
     }
